d013a9a9abd956e9c4fc92330af4a631be2ae389
andrija.dragojevic <andrija@coinis.com>
2023-08-15 11:33:06 +0200

    Added components: menu, footer, navbar, and almost finished the home page with charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+import "./styles/global.scss";
+
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+      ]
+    },
+  ]);
+
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router}>
+       
+    </RouterProvider>
   )
+ 
 }
 
 export default App
